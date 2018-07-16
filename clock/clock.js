8e62f399a14f9720e001731b3213cb0d590b1de9
Lindsey A. Erps <lae@booth-assoc.com>
2018-07-16 16:56:03 -0400

    clock

---
+++ clock/clock.js
@@ -0,0 +1,36 @@
+function at(hour, minute) {
+	return {
+		toString: function() {
+			let hr = '',
+				min = '';
+			if (!minute) {
+				minute = 0;
+			}
+			if (minute >= 60 || minute < 0) {
+				hour += Math.floor(minute / 60);
+				if (minute < 0) {
+					let bianca = Math.abs(minute % 60);
+					minute = 60 - bianca;
+				} else {
+					minute = minute % 60;
+				}
+			}
+			min = minute.toString();
+			if (minute < 10) {
+				min = '0' + min; 
+			}
+			if (hour < 0) {
+				let bruni = Math.abs(hour % 24);
+				hour = 24 - bruni;
+			}
+			hour = hour % 24; 
+			hr = hour.toString();
+			if (hour < 10) {
+				hr = '0' + hr;
+			}
+			return hr + ':' + min;
+		}
+	}
+}
+
+export default at;
